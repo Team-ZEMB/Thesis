ea5a9aa9d91ed95f23e007bde135faf1b42d3822
Brandon Kleiman <brandonwkleiman@gmail.com>
2017-03-30 14:33:28 -0700

    user route updates

--- api/controller.js
+++ api/controller.js
@@ -2,14 +2,23 @@ const request = require('request');
 const db = require('./schema');
 
 exports.createUser = function (req, res) {
-  // const newuser = db.User.build({
-  //   username: req.query.username,
-  //   current: null,
-  //   points: '0',
-  //   image: null,
-  // });
-  // newuser.save()
-  // .then((record) => {
-  //   res.send(record);
-  // });
+  const newuser = db.User.build({
+    username: req.query.username,
+    email: req.query.email,
+    firstName: req.query.firstName,
+    lastName: req.query.lastName,
+    profileImage: req.query.profileImage,
+    points: 0,
+  });
+  newuser.save()
+  .then((record) => {
+    res.send(record);
+  });
+};
+
+exports.signIn = function (req, res) {
+  db.User.findOne({ where: { unique: req.query.unique } })
+  .then((results) => {
+    res.send(results);
+  });
 };
