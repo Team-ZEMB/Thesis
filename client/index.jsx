0e3c50de1ce8e3723ed8973be94309c3d8b9ebbb
zc0311 <zc0311@gmail.com>
2017-03-30 19:48:21 -0700

    Auth restructuring - scrapped

--- client/index.jsx
+++ client/index.jsx
@@ -6,11 +6,20 @@ import reducers from './reducers/index';
 import { Router, Route } from 'react-router';
 import thunk from 'redux-thunk';
 import App from './components/App';
+<<<<<<< HEAD
+=======
+import Profile from './pages/Profile';
+import Login from './pages/Login';
+import createRoutes from './routes.jsx'
+import AuthService from './utils/AuthService'
+import config from '../environment'
+>>>>>>> Auth restructuring - scrapped
 
 const createHistory = require('history').createHashHistory;
 
 const hashHistory = createHistory();
 
+<<<<<<< HEAD
 // validate authentication for private routes
 
 ReactDOM.render(
@@ -19,4 +28,16 @@ ReactDOM.render(
       <Route path={'/'} component={App} />
     </Router>
   </Provider>
+=======
+const auth = new AuthService(config.AUTH_ID, config.AUTH_CLIENT);
+
+ReactDOM.render(
+  <div>
+    <Login auth={auth}/>
+  </div>
+>>>>>>> Auth restructuring - scrapped
   , document.getElementById('app'));
+
+  // Provider store={createStore(reducers, applyMiddleware(thunk))} >
+  //   <Router history={hashHistory} routes={createRoutes()} />
+  // </Provider>
---
+++ client/routes.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import { Route, IndexRedirect } from 'react-router';
+import Profile from './pages/Profile'
+import Login from './pages/Login';
+import config from '../environment';
+import App from './components/App'
+import AuthService from './utils/AuthService'
+
+const auth = new AuthService(config.AUTH_ID, config.AUTH_CLIENT);
+
+// validate authentication for private routes
+const requireAuth = (nextState, replace) => {
+  if (!auth.loggedIn()) {
+    replace({ pathname: '/login' });
+  }
+};
+
+const createRoutes = () => (
+  <Route path="/" component={App} auth={auth}>
+    <Route exact path="/" component={Profile} />
+    <Route path="/profile" component={Profile} onEnter={requireAuth} />
+    <Route path="/login" component={Login} auth={auth}/>
+  </Route>
+  );
+
+export default createRoutes;
