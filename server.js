adcd4f417e865c791126748d8c70b7832a80e917
Brandon Kleiman <brandonwkleiman@gmail.com>
2017-04-04 08:59:24 -0700

    server tests

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const morgan = require('morgan');
 const myRouter = require(`${__dirname}/api/router.js`);
 
 const app = express();
-// const cors = require('cors');
+const cors = require('cors');
 const port = process.env.PORT || 8000;
 
 //Middleware
@@ -14,19 +14,19 @@ app.use(morgan('combined'));
 app.use(express.static('./output'));
 app.use('/api', myRouter);
 
-// app.use(cors())
+app.use(cors())
 
-// app.all('*', (req, res, next) => {
-//   res.header('Access-Control-Allow-Origin', '*');
-//   res.header('Access-Control-Allow-Methods', 'PUT, GET, POST, DELETE, OPTIONS');
-//   res.header('Access-Control-Allow-Headers', 'accept, content-type, x-parse-application-id, x-parse-rest-api-key, x-parse-session-token');
+app.all('*', (req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', 'PUT, GET, POST, DELETE, OPTIONS');
+  res.header('Access-Control-Allow-Headers', 'accept, content-type, x-parse-application-id, x-parse-rest-api-key, x-parse-session-token');
 
-//   if (req.method === 'OPTIONS') {
-//     res.send(200);
-//   } else {
-//     next();
-//   }
-// });
+  if (req.method === 'OPTIONS') {
+    res.send(200);
+  } else {
+    next();
+  }
+});
 
 app.get('/*', (req, res) => {
   res.sendFile(`${__dirname}/output/index.html`);
