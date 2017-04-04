02962c90fa8da058c1e0e1494d20cae9e1cfba14
Brandon Kleiman <brandonwkleiman@gmail.com>
2017-04-04 11:01:14 -0700

    Update router.js

--- api/router.js
+++ api/router.js
@@ -2,20 +2,6 @@ const myRouter = require('express').Router();
 const controller = require('./controller');
 const db = require('./schema');
 
-myRouter.route('/userstest')
-  .post((req, res) => {
-    db.Users.findAll({ where: { authID: req.body.params.userID } })
-    .then((result) => {
-      if (result.length === 0) {
-        console.log("Body",req.body)
-        console.log("Paramus",req.params)
-      } else {
-        console.log("Body2",req.body)
-        console.log("Param2s",req.params)
-      }
-    });
-  });
-
 myRouter.route('/users')
   .post((req, res) => {
     db.Users.findAll({ where: { authID: req.body.params.userID } })
