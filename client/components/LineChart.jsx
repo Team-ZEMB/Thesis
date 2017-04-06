d81685680e363601c8ec75b609b1192de2dc20ba
zc0311 <zc0311@gmail.com>
2017-04-05 20:00:17 -0700

    Linear regression WORKS

--- client/components/LineChart.jsx
+++ client/components/LineChart.jsx
@@ -1,64 +1,95 @@
 import React from 'react';
 import { Line } from 'react-chartjs-2';
 import regression from 'regression';
+import { connect } from 'react-redux';
 
-var input = [[1,4],[1.5,null],[2,26],[0.5,2],[2.5, null],[3, null]];
+@connect((store) => {
+  return {
+    userdata: store.userdata,
+  };
+})
 
-var sorted = input.sort(function(a,b) {
-  return a[0] - b[0];
-});
+class LineChart extends React.Component{
 
-var result = regression('linear', sorted);
+    getData() {
+        var input = [];
 
-var unzip = function(array, index) {
-  var output = [];
-  for (var i = 0; i < array.length; i++) {
-    output.push(array[i][index]);
-  }
-  return output;
-}
+        var hist = this.props.userdata.history
 
-var xArray = unzip(result.points, 0);
-var yArray = unzip(result.points, 1);
+        console.log('history: ', hist);
 
-var data = {
-    labels: xArray,
-    datasets: [
-        {
-            label: "My First dataset",
-            fill: false,
-            lineTension: 0.1,
-            backgroundColor: "rgba(75,192,192,0.4)",
-            borderColor: "rgba(75,192,192,1)",
-            borderCapStyle: 'butt',
-            borderDash: [],
-            borderDashOffset: 0.0,
-            borderJoinStyle: 'miter',
-            pointBorderColor: "rgba(75,192,192,1)",
-            pointBackgroundColor: "#fff",
-            pointBorderWidth: 1,
-            pointHoverRadius: 5,
-            pointHoverBackgroundColor: "rgba(75,192,192,1)",
-            pointHoverBorderColor: "rgba(220,220,220,1)",
-            pointHoverBorderWidth: 2,
-            pointRadius: 1,
-            pointHitRadius: 10,
-            data: yArray,
-            spanGaps: false,
+        for (var j = 0; j < hist.length; j++) {
+            var tuple = [];
+            var minutes = this.props.userdata.history[j].duration / 60;
+            tuple.push(this.props.userdata.history[j].distance);
+            tuple.push(minutes);
+            input.push(tuple);
         }
-    ]
-};
+        console.log('input: ', input);
 
-export default React.createClass({
-    displayName: 'LineChart',
+        var sorted = input.sort(function(a,b) {
+            return a[0] - b[0];
+        });
+
+        console.log('sorted array: ', sorted);
+
+        var result = regression('linear', sorted);
+
+        console.log('regression result: ', result);
+
+        var unzip = function(array, index) {
+        var output = [];
+        for (var i = 0; i < array.length; i++) {
+            output.push(array[i][index]);
+        }
+            return output;
+        }
+
+        var xArray = unzip(result.points, 0);
+        var yArray = unzip(result.points, 1);
+
+        console.log('x data: ', xArray);
+        console.log('y data: ', yArray);
+
+        return ({
+            labels: xArray,
+            datasets: [
+                {
+                    label: "Linear regression: best-fit",
+                    fill: false,
+                    lineTension: 0.1,
+                    backgroundColor: "rgba(75,192,192,0.4)",
+                    borderColor: "rgba(75,192,192,1)",
+                    borderCapStyle: 'butt',
+                    borderDash: [],
+                    borderDashOffset: 0.0,
+                    borderJoinStyle: 'miter',
+                    pointBorderColor: "rgba(75,192,192,1)",
+                    pointBackgroundColor: "#fff",
+                    pointBorderWidth: 1,
+                    pointHoverRadius: 5,
+                    pointHoverBackgroundColor: "rgba(75,192,192,1)",
+                    pointHoverBorderColor: "rgba(220,220,220,1)",
+                    pointHoverBorderWidth: 2,
+                    pointRadius: 1,
+                    pointHitRadius: 10,
+                    data: yArray,
+                    spanGaps: false,
+                }
+            ]
+        });
+    }
 
     render() {
+        console.log("line graph data: ", this.getData());
         return (
             <div>
                 <h2>Time Estimates</h2>
                 <p>Predicts the time it will take you to run a number of miles based on your history</p>
-                <Line data={data} />
+                <Line data={this.getData()} />
             </div>
         );
     }
-});
+};
+
+export default LineChart
