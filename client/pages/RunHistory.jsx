4b97a8ba0120f3ab179434a05bd4f8c2fd08f578
Brandon Kleiman <brandonwkleiman@gmail.com>
2017-04-08 10:57:18 -0700

    rebase

--- client/pages/RunHistory.jsx
+++ client/pages/RunHistory.jsx
@@ -13,12 +13,32 @@ import {GoogleMapLoader, GoogleMap, Marker} from "react-google-maps";
 })
 
 export default class RunHistory extends React.Component {
+    constructor(props) {
+    super(props);
+    this.state = {
+      center: { 
+        lat: 33.9759, 
+        lng: -118.3907 
+      },
+      zoom: 13,
+    }
+  }
 
   componentWillMount() {
     if (!!localStorage.getItem("profile")) {
       this.props.dispatch(UserActions.signIn());
     }
   }
+  componentDidMount() {
+    // create the map, marker and infoWindow after the component has
+    // been rendered because we need to manipulate the DOM for Google =(
+    window.map = new google.maps.Map(this.refs.mapCanvas, {
+      zoom: this.state.zoom,
+      center: this.state.center
+    });
+    window.markerBounds = new google.maps.LatLngBounds();
+  }
+
   
   render() {
     var histArray = this.props.userdata.history.slice().reverse();
@@ -27,9 +47,12 @@ export default class RunHistory extends React.Component {
 
   return (
     <div>
+      <div className="map-view">
+      <div className="google-map" ref="mapCanvas"></div>
+      </div>
       <Card.Group itemsPerRow={2}>
-        {histArray.map(function(history) {
-          return <HistoryCard hist={history} />;
+        {histArray.map(function(history, idx) {
+          return <HistoryCard key={idx} hist={history} />;
         })}
       </Card.Group>
       </div>
