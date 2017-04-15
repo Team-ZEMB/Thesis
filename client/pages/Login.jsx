50a1083a8388652a26117b02cd4f4dc11bd35c03
iawen28 <marcie.ellen.anderson@gmail.com>
2017-04-15 15:08:23 -0700

    parallax more parallax

--- client/pages/Login.jsx
+++ client/pages/Login.jsx
@@ -32,14 +32,24 @@ export default class Login extends React.Component {
     var blueSquare = document.querySelector("#blueSquare");
     var greenPent = document.querySelector("#greenPent");
     var tealSq = document.querySelector("#tealSq");
+    var yellowSq = document.querySelector("#yellowSq");
+    var blueSq = document.querySelector("#blueSq");
+    var tealSq1 = document.querySelector("#tealSq1");
+    var salmonSq = document.querySelector("#salmonSq");
+    var rabbit = document.querySelector("#rabbit");
 
     function scrollLoop(e){
       var scroll = window.pageYOffset;
       setTranslate(0, scroll * -0.12, yellowCircle);
       setTranslate(scroll, 0, blueSquare);
       setTranslate(0, scroll * 0.2, greenPent);
-      setTranslate(0, scroll * -.9, tealSq);
-      requestAnimationFrame(scrollLoop)
+      setTranslate(0, scroll * -.1, tealSq);
+      setTranslate(0, scroll * -.2, yellowSq);
+      setTranslate(0, scroll * -.1, blueSq);
+      setTranslate(0, scroll * -.2, salmonSq);
+      setTranslate(0, scroll * -.1, tealSq1);
+      setTranslate(0, scroll * .7, rabbit);
+      requestAnimationFrame(scrollLoop);
     }
 
     function setTranslate(xPos, yPos, el) {
@@ -54,37 +64,41 @@ export default class Login extends React.Component {
         <img id="yellowCircle" src="assets/circle.png"/>
         <img id="greenPent" src="assets/greenpentagon.svg"/>
         <Button id="loginButton" color='teal' onClick={() => {AuthService.login(); }}>Login</Button>
-        <div className="bg" id="tealSq"> 
-        <img src="assets/teal.png" />
-        <img id="rabbit" style={{maxHeight: 200}} src="assets/rabbit.png"></img>
-        <h2>Welcome to Rabbit!</h2> 
+
+        <div className="bgr" id="tealSq"> 
+        <img src="assets/teal.png" className="childEl"/>
+        <div className="childEl">
+          <h2>Welcome to Rabbit!</h2> 
+          <img id="rabbit" style={{maxHeight: 200}} src="assets/rabbit.png"></img>
+        </div>
         </div>
 
-
         <div id="blueSquare">hi there!</div>
-                <Parallax className="parallaxImg" bgImage="assets/yellow.png" strength={300}>
-        <Background>
+        <div className="bgr" id="yellowSq" style={{marginTop: 395}}> 
+        <img src="assets/yellow.png" className="childEl"/>
+        <div className="childEl">
+          <h2>hihi!</h2> 
+        </div>
+        </div>
 
-        </Background>
-        </Parallax>
-        <Parallax className="parallaxImg" bgImage="assets/blue.png" strength={300}>
-        <Background>
-            <img style={{margin: 'auto', height: 690, width: 300 }} src='assets/teal.png' />
-          <div style={{ width: 900, height: 100, backgroundColor: 'orange' }}></div>
-          </Background>
-        </Parallax>
+        <div className="bgr" id="blueSq" style={{marginTop: 235}}> 
+        <img src="assets/blue.png" className="childEl"/>
+        <div className="childEl">
+          <img style={{margin: 'center', height: 690, width: 200 }} src='assets/teal.png' />
+        </div>
+        </div>
 
-        <Parallax className="parallaxImg" bgImage="assets/sy.png" strength={300}>
-        <Background>
+        <div className="bgr" id="salmonSq" style={{marginTop: 520}}> 
+        <img src="assets/sy.png" className="childEl"/>
+        <div className="childEl">
+        </div>
+        </div>
 
-        </Background>
-        </Parallax>
-
-        <Parallax className="parallaxImg" bgImage="assets/teal.png" strength={300}>
-        <Background>
-
-        </Background>
-        </Parallax>
+        <div className="bgr" id="tealSq1" style={{marginTop: 110}}> 
+        <img src="assets/teal.png" className="childEl"/>
+        <div className="childEl">
+        </div>
+        </div>
 
     </div>
     )
