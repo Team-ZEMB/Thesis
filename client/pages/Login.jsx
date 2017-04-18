a7928f91d0b7851f62e7468fa41cae3cd0718811
iawen28 <marcie.ellen.anderson@gmail.com>
2017-04-18 10:39:31 -0700

    more login progress

--- client/pages/Login.jsx
+++ client/pages/Login.jsx
@@ -38,7 +38,6 @@ export default class Login extends React.Component {
     setTimeout(scrollLoop, 0);
 
     var yellowCircle = document.querySelector("#yellowCircle");
-    var blueSquare = document.querySelector("#blueSquare");
     var greenHex = document.querySelector("#greenHex");
     var tealSq = document.querySelector("#tealSq");
     var yellowSq = document.querySelector("#yellowSq");
@@ -46,27 +45,26 @@ export default class Login extends React.Component {
     var tealSq1 = document.querySelector("#tealSq1");
     var salmonSq = document.querySelector("#salmonSq");
     var rabbit = document.querySelector("#rabbit");
+    var dl = document.querySelector("#dl");
     
     this.setState({
-      yellowMargin: 10,
+      yellowMargin: -10,
       blueMargin: -160,
-      salmonMargin: 140,
-      tealMargin: -280
+      salmonMargin: 100,
+      tealMargin: -270
     });
 
     function scrollLoop(e){
       var scroll = window.pageYOffset;
       setTranslate(0, scroll * -0.12, yellowCircle);
-      // setTranslate(scroll, 0, blueSquare);
-      rotate(scroll, 0, blueSquare);
-      // setTranslate(0, scroll * 0.2, greenHex);
-      rotate(0, scroll * 0.21, greenHex);
+      rotate(0, scroll * 0.1, greenHex);
+      // grow(scroll, dl);
       setTranslate(0, scroll * -.1, tealSq);
       setTranslate(0, scroll * -.2, yellowSq);
       setTranslate(0, scroll * -.1, blueSq);
       setTranslate(0, scroll * -.2, salmonSq);
       setTranslate(0, scroll * -.1, tealSq1);
-      setTranslate(0, scroll * .7, rabbit);
+      setTranslate(0, scroll * .4, rabbit);
       requestAnimationFrame(scrollLoop);
     }
 
@@ -76,11 +74,20 @@ export default class Login extends React.Component {
 
     function rotate(xPos, yPos, el) {
       if (xPos === 0) {
-        el.style.transform = 'translate3d(' + xPos + 'px, ' + yPos + 'px, 0px) rotate(' + yPos/4 +'deg)';
+        el.style.transform = 'translate3d(' + xPos + 'px, ' + yPos + 'px, 0px) rotate(' + yPos/2 +'deg)';
       } else {
         el.style.transform = 'translate3d(' + xPos + 'px, ' + yPos + 'px, 0px) rotate(' + xPos/3 +'deg)';
       }
     }
+
+    // function grow (scroll, el) {
+    //   var rect = el.getBoundingClientRect();
+    //   console.log('TOP: ', rect.top, rect.right, rect.bottom, rect.left);
+    //   if (rect.top > 5) {
+    //   el.style.transform = 'scale(' + 1.2 + ',' + 1.2 + ')';
+    // }
+    // }
+
   }
 
   render() {
@@ -91,36 +98,38 @@ export default class Login extends React.Component {
         <Button className="topLogin" color='teal' onClick={() => {AuthService.login(); }}>Login</Button>
 
         <div className="bgr" id="tealSq"> 
-        <img src="assets/teal.png" className="childEl"/>
+        <img src="assets/teal.png" className="childEl sq"/>
         <div className="childEl">
-          <h2>Welcome to Rabbit!</h2> 
-          <img id="rabbit" style={{maxHeight: 200}} src="assets/rabbit.png"></img>
+          <h2 id="welcome">Welcome to Rabbit!</h2> 
+          <img id="rabbit" style={{maxHeight: 400}} src="assets/rabbit.png"></img>
         </div>
         </div>
 
-        <div id="blueSquare">hi there!</div>
         <div className="bgr" id="yellowSq" style={{marginTop: this.state.yellowMargin}}> 
-        <img src="assets/yellow.png" className="childEl"/>
-        <div className="childEl">
-          <h2>hihi!</h2> 
+        <img src="assets/yellow.png" className="childEl sq"/>
+        <div className="childEl" style={{width: '90vw'}}>
+          <img src="assets/appDemo.gif" id="appDemo"></img>
+          <h2 id="dl"> Download running <br/>companion<br/><br/></h2>
+          <h3 className="description" id="tracks"> Save running time <br/>and route<br/></h3>
+          <h3 className="description"> Compete for best three-mile time <br/> alone or with your pack <br/></h3>
         </div>
         </div>
 
         <div className="bgr" id="blueSq" style={{marginTop: this.state.blueMargin}}> 
-        <img src="assets/blue.png" className="childEl"/>
+        <img src="assets/blue.png" className="childEl sq"/>
         <div className="childEl">
           <img style={{margin: 'center', height: 690, width: 200 }} src='assets/teal.png' />
         </div>
         </div>
 
         <div className="bgr" id="salmonSq" style={{marginTop: this.state.salmonMargin}}> 
-        <img src="assets/sy.png" className="childEl"/>
+        <img src="assets/sy.png" className="childEl sq"/>
         <div className="childEl">
         </div>
         </div>
 
         <div className="bgr1" id="tealSq1" style={{marginTop: this.state.tealMargin}}> 
-        <img src="assets/teal.png" className="childEl"/>
+        <img src="assets/teal1.png" className="childEl"/>
         <div className="childEl">
         </div>
         </div>
